3180cce7a575d3f38c456c8e52d02638759a1d2e
Felicià MAVIANE <maviane@gmail.com>
2020-03-28 11:17:19 +0100

    Added data in handling

--- src/ui_main.py
+++ src/ui_main.py
@@ -1,29 +1,69 @@
 import os
+from datetime import datetime as dt
+from datetime import timedelta as td
+from uuid import uuid4
+import json
 
 import pandas as pd
 import numpy as np
-import json
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, ObjectProperty, BooleanProperty
-from kivy.uix.button import Button
 from kivy.uix.behaviors import FocusBehavior
 from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 from kivy.uix.recycleboxlayout import RecycleBoxLayout
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.label import Label
+from kivy.uix.modalview import ModalView
+from kivy.uix.filechooser import FileChooserListView
 
 import drive as drive
 
 Config.set("graphics", "width", "800")
 Config.set("graphics", "height", "480")
+Config.set("graphics", "borderless", "1")
+
+TEST_MODE = True
+
+plant_data_path = os.path.join(".", "data", "plants_data.csv")
+if TEST_MODE:
+    jobs_file_path = os.path.join(".", "test_files", "test_jobs.json")
+    log_file_path = os.path.join(".", "test_files", "test_logs.txt")
+else:
+    jobs_file_path = os.path.join(".", "data", "jobs_data.json")
+    log_file_path = os.path.join(".", "data", "logs.txt")
+
+if os.path.isfile(plant_data_path):
+    plant_data = pd.read_csv(plant_data_path)
+else:
+    plant_data = pd.DataFrame(
+        columns=["experiment", "plant_name", "position", "allow_capture"]
+    )
+
+if os.path.isfile(jobs_file_path):
+    with open(jobs_file_path, "r") as f:
+        jobs_data = json.load(f)
+else:
+    jobs_data = {"jobs": []}
+
+if os.path.isfile(log_file_path):
+    with open(log_file_path, "r") as f:
+        log_data = f.readlines()
+else:
+    log_data = []
 
 
 class RootWidget(BoxLayout):
     pass
+
+
+class ModalDialog(ModalView):
+
+    modal_dialog_title = ObjectProperty(None)
+    modal_dialog_body = ObjectProperty(None)
 
 
 class SelectableRecycleBoxLayout(
@@ -38,6 +78,7 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
     index = None
     selected = BooleanProperty(False)
     selectable = BooleanProperty(True)
+    guid = StringProperty("")
 
     def refresh_view_attrs(self, rv, index, data):
         """ Catch and handle the view changes """
@@ -59,13 +100,7 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
             and hasattr(rv, "page")
             and hasattr(rv.page, "update_data")
         ):
-            rv.page.update_data(self.text)
-
-
-root_folder = "./data/" if os.path.isdir("./data/") else "../data"
-plant_data = pd.read_csv(os.path.join(root_folder, "plants_data.csv"))
-with open(os.path.join(root_folder, "jobs_data.json"), "r") as f:
-    jobs_data = json.load(f)
+            rv.page.update_data(guid=self.guid)
 
 
 class MyPageManager(ScreenManager):
@@ -104,9 +139,26 @@ class StartUpPage(Screen):
     back_text = "< Exit"
     title_text = "Start up"
     info_text = "Welcome to RobotRacine control center"
+    modal_dialog = None
+
+    def dialog_callback(self, instance):
+        if instance.modal_result == 1:
+            plant_data.to_csv(plant_data_path, index=False)
+            with open(os.path.join(".", "data", "logs.txt"), "w") as f:
+                f.write("\n".join(log_data))
+            with open(os.path.join(".", "data", "jobs_data.json"), "w") as f:
+                json.dump(jobs_data, f, indent=2)
+            App.get_running_app().stop()
+        return False
 
     def back(self):
-        App.get_running_app().stop()
+        self.modal_dialog = ModalDialog()
+        self.modal_dialog.modal_dialog_title.text = "Confirmation required"
+        self.modal_dialog.modal_dialog_body.text = (
+            "Quit program?\nAll jobs will be stopped."
+        )
+        self.modal_dialog.bind(on_dismiss=self.dialog_callback)
+        self.modal_dialog.open()
 
 
 class MyScreen(Screen):
@@ -123,18 +175,22 @@ class ManualRoot(MyScreen):
     info_text = "Take control of the robot"
 
     def go_home(self):
-        self.ids["mo_home"].text = "Going Home"
+        self.manager.lbl_status.text = "Going Home"
         drive.go_home()
-        self.ids["mo_home"].text = "Went Home"
+        self.manager.lbl_status.text = "Went Home"
 
     def run(self):
+        self.manager.lbl_status.text = "Engine On"
         drive.run()
 
     def go_next(self):
+        self.manager.lbl_status.text = "Setting next plant"
         drive.go_next()
+        self.manager.lbl_status.text = "Next plant set"
 
     def stop(self):
         drive.stop()
+        self.manager.lbl_status.text = "Engine stopped"
 
 
 class ManualCapture(MyScreen):
@@ -156,6 +212,53 @@ class Jobs(MyScreen):
     info_text = "All about jobs"
 
 
+class PlantSelector(ModalView):
+
+    available_plants = ObjectProperty(None)
+    selected_plants = ObjectProperty(None)
+    selected_plants_list = []
+    job = None
+
+    def update_list_views(self):
+        self.ids["available_plants"].data = [
+            {"text": j}
+            for j in [
+                plant
+                for plant in plant_data.plant_name.unique()
+                if plant not in self.selected_plants_list
+            ]
+        ]
+        self.ids["selected_plants"].data = [
+            {"text": j} for j in self.selected_plants_list
+        ]
+
+    def add_to_selection(self):
+        selected_nodes = self.ids[
+            "available_plants"
+        ].layout_manager.selected_nodes
+        self.ids["available_plants"].layout_manager.selected_nodes = []
+        if selected_nodes:
+            self.selected_plants_list.extend(
+                [
+                    self.ids["available_plants"].data[i]["text"]
+                    for i in selected_nodes
+                ]
+            )
+            self.update_list_views()
+
+    def remove_from_selection(self):
+        selected_nodes = [
+            self.ids["selected_plants"].data[i]["text"]
+            for i in self.ids["selected_plants"].layout_manager.selected_nodes
+        ]
+        self.ids["selected_plants"].layout_manager.selected_nodes = []
+        if selected_nodes:
+            self.selected_plants_list = [
+                p for p in self.selected_plants_list if p not in selected_nodes
+            ]
+            self.update_list_views()
+
+
 class JobsManage(MyScreen):
     back_text = "< Back"
     back_target = "jobs"
@@ -166,37 +269,138 @@ class JobsManage(MyScreen):
     job_description = ObjectProperty(None)
 
     def init_jobs(self):
-        self.jobs_list.data = [{"text": j["name"]} for j in jobs_data["jobs"]]
-        print("init jobs")
+        self.jobs_list.data = [
+            {"text": j["name"], "guid": j["guid"]} for j in jobs_data["jobs"]
+        ]
 
     def get_current_description(self):
         return "description"
 
-    def update_data(self, name):
+    def get_job(self, guid):
         for job in jobs_data["jobs"]:
-            if job["name"] == name:
-                self.job_description.input_text = job["description"]
-                self.job_owner.input_text = job["owner"]
-                self.job_mail_list.input_text = "; ".join(job["mail_to"])
-                self.time_mode.text = job["repetition_mode"]
-                self.time_value.text = str(job["repetition_value"])
-                self.time_unit.text = job["repetition_unit"]
-                self.date_start.text = job["timestamp_start"]
-                self.date_end.text = job["timestamp_end"]
-                self.job_plants.input_text = "; ".join(job["plants"])
-                continue
+            if job["guid"] == guid:
+                return job
+        else:
+            return None
 
-    def save_job(self):
-        print("Save job")
+    def get_job_index(self, guid):
+        for i, job in enumerate(jobs_data["jobs"]):
+            if job["guid"] == guid:
+                return i
+        else:
+            return -1
 
-    def resume_job(self):
-        print("Save job")
+    def new_job(self):
+        if not jobs_data:
+            jobs_data["jobs"] = []
+        jobs_data["jobs"].append(
+            {
+                "name": "Job " + dt.now().strftime("%Y%m%d %H:%M:%S"),
+                "state": "active",
+                "guid": str(uuid4()),
+                "description": "",
+                "owner": "",
+                "mail_to": "",
+                "repetition_mode": "every",
+                "repetition_value": 6,
+                "repetition_unit": "hours",
+                "timestamp_start": dt.now().strftime("%Y%m%d %H:%M:%S"),
+                "timestamp_end": (dt.now() + td(days=30)).strftime(
+                    "%Y%m%d %H:%M:%S"
+                ),
+                "plants": "",
+            }
+        )
+        self.init_jobs()
+        self.update_data(guid=jobs_data["jobs"][-1]["guid"])
 
-    def pause_job(self):
-        print("Pause job")
+    def update_data(self, guid):
+        job = self.get_job(guid=guid)
+        if job is None:
+            return
+        self.job_name.input_text = job["name"]
+        self.job_guid = guid
+        self.job_state.text = "" if job["state"] == "active" else "paused"
+        self.job_description.input_text = job["description"]
+        self.job_owner.input_text = job["owner"]
+        self.job_mail_list.input_text = "; ".join(job["mail_to"])
+        self.time_mode.text = job["repetition_mode"]
+        self.time_value.text = str(job["repetition_value"])
+        self.time_unit.text = job["repetition_unit"]
+        self.date_start.text = job["timestamp_start"]
+        self.date_end.text = job["timestamp_end"]
+        self.job_plant_list.text = ";".join(job["plants"])
 
-    def delete_job(self):
-        print("Delete job")
+    def save_job(self, guid):
+        index = self.get_job_index(guid=guid)
+        if index < 0:
+            return
+        jobs_data["jobs"][index]["name"] = self.job_name.text_holder.text
+        jobs_data["jobs"][index]["state"] = (
+            "paused" if self.job_state.text == "paused" else "active"
+        )
+        jobs_data["jobs"][index][
+            "description"
+        ] = self.job_description.text_holder.text
+        jobs_data["jobs"][index]["owner"] = self.job_owner.text_holder.text
+        jobs_data["jobs"][index][
+            "mail_to"
+        ] = self.job_mail_list.text_holder.text.replace(" ", "").split(";")
+        jobs_data["jobs"][index]["repetition_mode"] = self.time_mode.text
+        try:
+            jobs_data["jobs"][index]["repetition_value"] = int(
+                self.time_value.text
+            )
+        except:
+            jobs_data["jobs"][index]["repetition_value"] = 0
+        jobs_data["jobs"][index]["repetition_unit"] = self.time_unit.text
+        jobs_data["jobs"][index]["timestamp_start"] = self.date_start.text
+        jobs_data["jobs"][index]["timestamp_end"] = self.date_end.text
+
+    def resume_job(self, guid):
+        job = self.get_job(guid=guid)
+        if job is None:
+            return
+        job["state"] = "active"
+        self.update_data(guid=guid)
+
+    def pause_job(self, guid):
+        job = self.get_job(guid=guid)
+        if job is None:
+            return
+        job["state"] = "paused"
+        self.update_data(guid=guid)
+
+    def delete_job(self, guid):
+        index = self.get_job_index(guid=guid)
+        if index < 0:
+            return
+        jobs_data["jobs"].pop(index)
+        self.init_jobs()
+        self.update_data(guid=jobs_data["jobs"][-1]["guid"])
+
+    def close_plant_selection(self, instance):
+        if instance.modal_result == 1:
+            selected_nodes = [
+                instance.ids["selected_plants"].data[i]["text"]
+                for i in instance.ids[
+                    "selected_plants"
+                ].layout_manager.selected_nodes
+            ]
+            instance.job["plants"] = selected_nodes
+            self.update_data(guid=instance.job["guid"])
+        return False
+
+    def select_plants(self, guid):
+        job = self.get_job(guid=guid)
+        if job is None:
+            return
+        self.plant_selector = PlantSelector()
+        self.plant_selector.selected_plants_list = job["plants"][:]
+        self.plant_selector.job = job
+        self.plant_selector.update_list_views()
+        self.plant_selector.bind(on_dismiss=self.close_plant_selection)
+        self.plant_selector.open()
 
 
 class JobsLog(MyScreen):
@@ -205,6 +409,13 @@ class JobsLog(MyScreen):
     title_text = "Jobs log"
     info_text = "Logs, logs everywhere"
 
+    def init_logs(self):
+        self.log_text.text = "\n".join(reversed(log_data))
+
+
+class FileLoader(ModalView):
+    file_name = ObjectProperty(None)
+
 
 class DataIn(MyScreen):
     back_text = "< Back"
@@ -212,12 +423,45 @@ class DataIn(MyScreen):
     title_text = "Data In"
     info_text = "View/add/remove Data In files"
 
+    experiments_list = ObjectProperty(None)
 
-class DataInEdit(MyScreen):
-    back_text = "< Back"
-    back_target = ""
-    title_text = "Edit Data In"
-    info_text = "Edit Data In"
+    def init_experiments(self):
+        self.experiments_list.data = [
+            {"text": j} for j in plant_data.experiment.unique()
+        ]
+
+    def close_file_selection(self, instance):
+        if instance.modal_result == 1:
+            try:
+                new_df = pd.read_csv(instance.ids["file_name"].text)
+            except Exception as e:
+                log_data.append(
+                    f"{dt.now().strftime('%Y%m%d %H:%M:%S')} - Failed to load data in because {repr(e)}"
+                )
+            else:
+                global plant_data
+                plant_data = pd.concat((plant_data, new_df)).drop_duplicates()
+                self.init_experiments()
+        return False
+
+    def load_file(self):
+        self.file_loader = FileLoader()
+        self.file_loader.bind(on_dismiss=self.close_file_selection)
+        self.file_loader.open()
+
+    def remove_experiment(self):
+        global plant_data
+        plant_data = plant_data[
+            ~plant_data.experiment.isin(
+                [
+                    self.ids["experiments_list"].data[i]["text"]
+                    for i in self.ids[
+                        "experiments_list"
+                    ].layout_manager.selected_nodes
+                ]
+            )
+        ]
+        self.init_experiments()
 
 
 class RobotRacineApp(App):
